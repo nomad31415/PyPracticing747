a057dbfd9442056814e21a89a0f1cfa70137fe17
igor z <mcatoeff@gmail.com>
2023-10-08 12:16:13 -0700

    added leap.zig

--- Zig/exercises/practice/leap/leap.zig
+++ Zig/exercises/practice/leap/leap.zig
@@ -1,4 +1,15 @@
 pub fn isLeapYear(year: u32) bool {
-    _ = year;
-    @compileError("please implement the isLeapYear function");
+
+	if (n % 400 == 0)
+        	return true;
+
+	if (n % 100 == 0)
+	        return false;
+
+	if (n % 4 == 0 )
+		return true;
+
+	return false;
+
+
 }
