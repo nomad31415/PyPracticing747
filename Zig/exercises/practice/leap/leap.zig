9168badee146d2e8c2345978791459d93ae14c81
igor z <mcatoeff@gmail.com>
2023-10-08 12:58:36 -0700

    upgraded leap.zig

--- Zig/exercises/practice/leap/leap.zig
+++ Zig/exercises/practice/leap/leap.zig
@@ -1,15 +1,17 @@
 pub fn isLeapYear(year: u32) bool {
 
-	if (n % 400 == 0)
+//	if (n % 400 == 0)
         	return true;
+//
+//	if (n % 100 == 0)
+//	        return false;
+//
+//	if (n % 4 == 0 )
+//		return true;
+//
+//	return false;
 
-	if (n % 100 == 0)
-	        return false;
-
-	if (n % 4 == 0 )
-		return true;
-
-	return false;
+	return (n % 4 == 0 and (n % 100 != 0 or n % 400 == 0));
 
 
 }
