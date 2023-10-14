e50330b5e567c20ce69edfef5be16710b4719a14
igor z <mcatoeff@gmail.com>
2023-10-14 11:05:10 -0700

    added collatz

--- Zig/exercises/practice/collatz-conjecture/collatz_conjecture.zig
+++ Zig/exercises/practice/collatz-conjecture/collatz_conjecture.zig
@@ -1,4 +1,32 @@
+//pub fn steps(number: usize) anyerror!usize {
+//    _ = number;
+//    @compileError("please implement the steps function");
+//}
+
+
+
+pub const ComputationError = error{IllegalArgument,};
+
 pub fn steps(number: usize) anyerror!usize {
-    _ = number;
-    @compileError("please implement the steps function");
+
+    if (number < 1) return ComputationError.IllegalArgument;
+
+    var count: usize = 0;
+    var current: usize = number;
+
+    while (current != 1) {
+	if (current % 2 == 0) {
+	    current /= 2;
+	} else {
+	    current = 3 * current + 1;
+	}
+
+	count += 1;
+     }
+
+
+    return count;
 }
+
+
+
