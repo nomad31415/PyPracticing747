b817c88f2890fb106c96e624e6690fd44651c7b8
igor z <mcatoeff@gmail.com>
2023-11-01 12:22:07 -0700

    added rust files

---
+++ Rust/hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\git-sdk-64\Projects\.ssh\Clj2\cl_Test\Py\PyPracticing747\Rust\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\git-sdk-64\Projects\.ssh\Clj2\cl_Test\Py\PyPracticing747\Rust\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ Rust/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\git-sdk-64\Projects\.ssh\Clj2\cl_Test\Py\PyPracticing747\Rust\hello_cargo\target\debug\hello_cargo.exe: C:\git-sdk-64\Projects\.ssh\Clj2\cl_Test\Py\PyPracticing747\Rust\hello_cargo\src\main.rs
