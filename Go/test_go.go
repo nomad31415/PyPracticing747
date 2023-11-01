326ea1e3acddf3656db8be6fe776d144e89c607d
igor z <mcatoeff@gmail.com>
2023-11-01 13:00:24 -0700

    go chan

--- Go/test_go.go
+++ Go/test_go.go
@@ -1,17 +1,31 @@
 package main
 
-
 import "fmt"
 
+func gorutinIt(s []int, c chan int) {
 
+	sum := 0
 
+	for _, v := range s {
+		sum += v
+	}
 
-
-func  main() {
-
-
-fmt.Println("test")
-
+	c <- sum
 }
 
+func main() {
 
+	arr := []int{1, 2, 3, 4, 5, 6, 7}
+
+	c := make(chan int)
+
+	go gorutinIt(arr[:len(arr)/2], c)
+	go gorutinIt(arr[len(arr)/2:], c)
+
+	x, y := <-c, <-c
+
+	fmt.Println(x + y)
+
+	fmt.Println("test")
+
+}
