eb8c6382ebf6b1b8a0efcce7a989d06a2977fed0
igor z <mcatoeff@gmail.com>
2023-12-05 16:56:14 -0800

    added TestGo1

---
+++ testGo1.go
@@ -0,0 +1,24 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+type Pol struct {
+	data int
+	}
+
+	func NewKoko() *Pol {
+		return &Pol{}
+		}
+
+		func main() {
+
+			bo := NewKoko()
+
+				bo.data = 5
+					fmt.Println(bo.data)
+
+						fmt.Println("Hello, 世界")
+						}
+
